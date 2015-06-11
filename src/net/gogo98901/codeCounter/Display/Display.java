237bef889891b0e9286788da5d7522f25db5b8e3
GOGO98901 <gogo98901@live.co.uk>
2015-06-11 17:56:49 +0100

    Grouped everything on contentpane

--- src/net/gogo98901/codeCounter/Display/Display.java
+++ src/net/gogo98901/codeCounter/Display/Display.java
@@ -72,45 +72,47 @@ public class Display extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
+		{
+			searchField = new JTextField();
+			searchField.addKeyListener(new KeyAdapter() {
+				public void keyPressed(KeyEvent event) {
+					if (event.getKeyCode() == KeyEvent.VK_ENTER) search();
+				}
+			});
+			searchField.setBounds(10, 11, 376, 20);
+			searchField.setText(new File(System.getProperty("user.home")).getAbsolutePath());
+			contentPane.add(searchField);
+			searchField.setColumns(10);
 
-		searchField = new JTextField();
-		searchField.addKeyListener(new KeyAdapter() {
-			public void keyPressed(KeyEvent event) {
-				if (event.getKeyCode() == KeyEvent.VK_ENTER) search();
-			}
-		});
-		searchField.setBounds(10, 11, 376, 20);
-		searchField.setText(new File(System.getProperty("user.home")).getAbsolutePath());
-		contentPane.add(searchField);
-		searchField.setColumns(10);
+			btnSearch = new JButton("Scan Dir");
+			btnSearch.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent event) {
+					search();
+				}
+			});
+			btnSearch.setBounds(495, 10, 89, 23);
+			contentPane.add(btnSearch);
+		}
+		{
+			btnBrowse = new JButton("Browse");
+			btnBrowse.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					showSearchWindow();
+				}
+			});
+			btnBrowse.setBounds(396, 10, 89, 23);
+			contentPane.add(btnBrowse);
+		}
+		{
+			result = new JTextPane();
+			result.setEditable(false);
+			result.setBounds(0, 0, 6, 20);
 
-		btnBrowse = new JButton("Browse");
-		btnBrowse.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				showSearchWindow();
-			}
-		});
-		btnBrowse.setBounds(396, 10, 89, 23);
-		contentPane.add(btnBrowse);
-
-		btnSearch = new JButton("Scan Dir");
-		btnSearch.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				search();
-			}
-		});
-		btnSearch.setBounds(495, 10, 89, 23);
-		contentPane.add(btnSearch);
-
-		result = new JTextPane();
-		result.setEditable(false);
-		result.setBounds(0, 0, 6, 20);
-
-		ScrollPane scrollPane = new ScrollPane();
-		scrollPane.add(result);
-		scrollPane.setBounds(10, 37, 574, 374);
-		contentPane.add(scrollPane);
-
+			ScrollPane scrollPane = new ScrollPane();
+			scrollPane.add(result);
+			scrollPane.setBounds(10, 37, 574, 374);
+			contentPane.add(scrollPane);
+		}
 		{
 			fieldLines = new JTextField("0");
 			fieldLines.setBounds(10, 417, 86, 20);
@@ -183,14 +185,16 @@ public class Display extends JFrame {
 			labelDirExcluded.setBounds(338, 446, 147, 22);
 			contentPane.add(labelDirExcluded);
 		}
-		btnConfig = new JButton("Config");
-		btnConfig.setBounds(495, 447, 89, 23);
-		btnConfig.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				config.show();
-			}
-		});
-		contentPane.add(btnConfig);
+		{
+			btnConfig = new JButton("Config");
+			btnConfig.setBounds(495, 447, 89, 23);
+			btnConfig.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent event) {
+					config.show();
+				}
+			});
+			contentPane.add(btnConfig);
+		}
 	}
 
 	private void showSearchWindow() {
